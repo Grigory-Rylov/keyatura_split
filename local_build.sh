691e10b47b3dfb27acb69725a322fa980a8f6669
ggrocer <esyunin.maks@mail.ru>
2025-08-02 10:28:50 +0300

    added encoder on dongle

--- local_build.sh
+++ local_build.sh
@@ -75,9 +75,9 @@ mkdir -p $CURRENT_DIR/build
 pushd $ZMK_APP_DIR
 
 # build_halves left
-build_halves right
+# build_halves right
 build_dongle 
-build_mouse
+# build_mouse
 # build_reset
 
 deactivate
